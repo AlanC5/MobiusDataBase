595469d18220cf06f338605290b13344f04b53fd
Christopher Yang <cyang001@citymail.cuny.edu>
2016-05-30 00:11:30 -0400

    Setup login page

--- djmobius/djmobius/settings.py
+++ djmobius/djmobius/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mobius.apps.MobiusConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
