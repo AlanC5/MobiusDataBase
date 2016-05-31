8bdab37bae5818aa196c5bf0e8acf542ae917b8b
Christopher Yang <cyang001@citymail.cuny.edu>
2016-05-30 23:41:03 -0400

    Create UserProfile model

--- djmobius/mobius/models.py
+++ djmobius/mobius/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
+from django.core.validators import URLValidator
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    """
+    A UserProfile Model that contains the following attributes:
+        - fullName
+        - email
+        - imageUrl
+    """
+    fullName = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    imageUrl = models.TextField(validators=[URLValidator()])
+
+    def __unicode__(self):
+        return self.email
