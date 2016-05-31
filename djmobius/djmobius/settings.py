0b950acd1c0848dd452d02b26fa1ddbeefa9b6db
Christopher Yang <cyang001@citymail.cuny.edu>
2016-05-30 23:01:17 -0400

    Set up mysql db

--- djmobius/djmobius/settings.py
+++ djmobius/djmobius/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djmobius.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mobiusData',
+        'USER': 'ebroot',
+        'PASSWORD': '12345678',
+        'HOST': 'aaavvlvgxl15b5.cn04yza62psx.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
